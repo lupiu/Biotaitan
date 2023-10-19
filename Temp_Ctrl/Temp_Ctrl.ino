ac0350d307c60013c71f0eaf4bd065ddd13c5214
silva_lin <silva_lin@sanwa-eng.com.tw>
2023-10-19 16:11:32 +0800

    add base temp holdtime

--- Temp_Ctrl/Temp_Ctrl.ino
+++ Temp_Ctrl/Temp_Ctrl.ino
@@ -23,13 +23,17 @@
 #define THERM_BASE_B 3455 //0~105C
 #define THERM_MIN_R 127
 
-#define BASE_TEMP 55
+#define BASE_ENABLE 1 //1:enable 0:disable //僅第一個Cycle有作用
+#define BASE_TEMP 55 //degrees Celsius
+#define BASE_HOLDTIME 2000 //unit:ms
+
 #define TOP_TEMP 95
 #define BOTTOM_TEMP 65
-
 #define TOP_HOLDTIME 2000
 #define BOTTOM_HOLDTIME 5000
 #define TEMP_CYCLE 12
+
+#define CYCLE_STATUS 3 //0:BASE 1:BOTTOM 2:TOP 3:All close
 
 //===================//
 typedef struct
@@ -51,6 +55,13 @@ typedef struct
 _TempCtrl g_TempData = {25, 25, 0};
 _PidData g_PidData;
 PID g_Temp_PID(&g_PidData.Input, &g_PidData.Output, &g_PidData.SetPoint, 2, 5, 1, DIRECT);
+
+//===================//
+void TEMP_AllOff(void)
+{
+  analogWrite(HEATER_CTRL, 0);
+  analogWrite(FAN_CTRL, 0);
+}
 
 //===================//
 float TEMP_ReadTemperature(uint8_t pin)
@@ -106,6 +117,35 @@ void TEMP_TempCtrl(float temp_c, uint8_t pin)
 }
 
 //--------------------------------------------------
+uint8_t RunToTemp(float temp_c, uint8_t dir, uint16_t holdtime)
+{
+  static double achieve_time;
+  static uint8_t achieve_flag = 0;
+  uint8_t done = 0;
+  
+  TEMP_TempCtrl(temp_c, NTC_TS3);
+  if (achieve_flag == 0)
+  {
+    if ((dir == HIGH && g_TempData.PresentTemp_C >= temp_c) ||
+       (dir == LOW && g_TempData.PresentTemp_C <= temp_c))
+    {
+      achieve_time = millis();
+      achieve_flag = 1;
+    }
+  }
+  else
+  {
+    if ((millis()  - achieve_time) >= holdtime)
+    {
+      achieve_flag = 0;
+      done = 1;
+    }
+  }
+  
+  return done;
+}
+
+//--------------------------------------------------
 void setup() {
 	Serial.begin(115200);
 
@@ -119,16 +159,16 @@ void setup() {
   pinMode(IO0, INPUT_PULLUP);
 
   PID_Initial();
+  TEMP_AllOff();
 }
 
 //--------------------------------------------------
 void loop()
 {
   uint8_t mode;
-  double achieve_time;
-  static uint8_t achieve_flag = 0;
   static uint8_t cycle_cnt = 0;
-  static uint8_t dir = 1;
+  static uint8_t dir = HIGH;
+  static uint8_t base_initial = LOW;
 
   mode = digitalRead(IO2);
   if (mode == 1)
@@ -142,53 +182,53 @@ void loop()
   { 
     if (digitalRead(IO0) == 1)
     {
-      TEMP_TempCtrl(BASE_TEMP, NTC_TS3);
       cycle_cnt = 0;
-      dir = 1;
+      dir = HIGH;
+      base_initial = LOW;
     }
     else
-    {
-      if (cycle_cnt < TEMP_CYCLE)
+    {    
+      if (BASE_ENABLE == 1 && base_initial == LOW)
       {
-        if (dir == 1)
+        if (RunToTemp(BASE_TEMP, HIGH, BASE_HOLDTIME) == 1)
+        base_initial = HIGH;
+      }
+      else
+      {
+        if (cycle_cnt < TEMP_CYCLE)
         {
-          TEMP_TempCtrl(TOP_TEMP, NTC_TS3);
-          if (achieve_flag == 0)
+          if (dir == HIGH)
           {
-            if (g_TempData.PresentTemp_C >= TOP_TEMP)
+            if (RunToTemp(TOP_TEMP, dir, TOP_HOLDTIME) == 1)
             {
-              achieve_time = millis();
-              achieve_flag = 1;
+              dir = LOW;
             }
           }
           else
           {
-            if ((millis()  - achieve_time) >= TOP_HOLDTIME)
+            if (RunToTemp(BOTTOM_TEMP, dir, BOTTOM_HOLDTIME) == 1)
             {
-              dir = 0;
-              achieve_flag = 0;
+              dir = HIGH;
+              cycle_cnt++;
             }
           }
         }
         else
         {
-          TEMP_TempCtrl(BOTTOM_TEMP, NTC_TS3);
-          if (achieve_flag == 0)
+          switch (CYCLE_STATUS)
           {
-            if (g_TempData.PresentTemp_C <= BOTTOM_TEMP)
-            {
-              achieve_time = millis();
-              achieve_flag = 1;
-            }
-          }
-          else
-          {
-            if ((millis()  - achieve_time) >= BOTTOM_HOLDTIME)
-            {
-              dir = 1;
-              achieve_flag = 0;
-              cycle_cnt++;
-            }
+            case 0 : 
+              TEMP_TempCtrl(BASE_TEMP, NTC_TS3);
+            break;
+            case 1 : 
+              TEMP_TempCtrl(BOTTOM_TEMP, NTC_TS3);
+            break;
+            case 2 : 
+              TEMP_TempCtrl(TOP_TEMP, NTC_TS3);
+            break;
+            default : 
+              TEMP_AllOff();
+            break;
           }
         }
       } 
